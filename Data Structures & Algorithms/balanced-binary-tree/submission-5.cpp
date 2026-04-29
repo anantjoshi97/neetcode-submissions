8a7ce80c0e19ba838d884c87ef85b7920788e2ff
Anant Joshi <anantjoshi97@users.noreply.github.com>
2026-04-29 12:57:43 -0500

    Add: balanced-binary-tree - submission-5

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-5.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int get_ht (TreeNode* root, bool & balance){
+        // f (balance == false) {return -1;}
+        if (root==nullptr){return -1;}
+        int ht_left = get_ht(root->left, balance);
+        if (balance == false){return -1;}
+        int ht_right = get_ht(root->right, balance);        
+        if (balance == false){return -1;}
+        balance = balance && (std::abs(ht_left - ht_right)<=1);
+        // if (balance == false){return -1;}
+        int ht = 
+            std::max(ht_left,ht_right) + 1;
+        return ht;
+    }
+    bool isBalanced(TreeNode* root) {        
+        if (root==nullptr){return true;}
+        bool balance = true;
+        int ht = get_ht(root, balance);
+        return balance;
+        //int ht_left = get_ht(root->left);
+        //int ht_right = get_ht(root->right);
+        //bool b1 = (std::abs(ht_left - ht_right)<=1);
+        //if (b1 && isBalanced(root->left) && isBalanced(root->right)){
+        //    return true;
+        //}
+        //return false;
+    }
+};
