049f06253cda7f59c9626741f089ceaba6797fcb
Anant Joshi <anantjoshi97@users.noreply.github.com>
2026-04-30 14:29:59 -0500

    Add: path-sum - submission-3

---
+++ Data Structures & Algorithms/path-sum/submission-3.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root, int sum, int target) {
+        if (root==nullptr){return false;}
+        int s2 = sum + root-> val;
+        if (s2==target && 
+            root->left==nullptr && root->right==nullptr)
+            {return true;}
+        if(helper(root->left,s2,target)){return true;}
+        if(helper(root->right,s2,target)){return true;}
+        return false;
+    }
+
+    bool hasPathSum2(TreeNode* root, int targetSum) {
+        return helper(root,0,targetSum);
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root==nullptr){return false;}
+
+        std::stack<std::pair<TreeNode*,int>> stk;
+        int rem = targetSum;
+        stk.push({root,rem});
+
+        while(!stk.empty()){
+            rem = stk.top().second;
+            TreeNode* current = stk.top().first;
+            stk.pop();
+            if (current==nullptr){continue;}
+            rem -= current->val;
+            if (rem==0 && current->left==nullptr 
+                && current->right==nullptr){
+                return true;
+            }
+            stk.push({current->right,rem});
+            stk.push({current->left,rem});
+        }
+
+        return false;
+    }
+};
