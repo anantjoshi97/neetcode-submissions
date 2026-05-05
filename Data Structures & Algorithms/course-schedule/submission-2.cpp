0402d1bde75ddcc9a2f46bf5ae6718fe4988bdda
Anant Joshi <anantjoshi97@users.noreply.github.com>
2026-05-05 18:16:40 -0500

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        if (numCourses==1){return true;}
+        //std::vector<bool> courses;
+        std::unordered_map<int,vector<int>> in, out;
+        std::unordered_set<int> history;
+        std::queue<int> q;
+
+        for (int i=0; i<numCourses; i++){
+            history.insert(i);
+        }
+
+        for (vector<int> p : prerequisites){
+            int c = p[0];
+            history.erase(c);
+            q.push(c);
+            int pr = p[1];
+            in[c].push_back(pr);
+            out[pr].push_back(c);
+        }
+
+        //if (history.empty()){return false;}
+        //else if(history.size()==numCourses){return true;}
+
+        // for (int pre : history){
+        //     q.push(pre);
+        // }
+        int new_sz = 0;
+        int sz = q.size();
+        while(!q.empty() && new_sz < sz){
+            sz = q.size();
+            for (int i=0; i<sz; i++){
+                int c = q.front();
+                q.pop();
+                bool can_do = true;
+                for (int pre : in[c]){
+                    can_do = history.count(pre);
+                    if (!can_do){break;}
+                }
+                if (can_do){history.insert(c);}
+                else{q.push(c);}
+            }
+            new_sz = q.size();
+        }
+        
+        return(history.size()==numCourses);
+
+
+
+    }
+};
