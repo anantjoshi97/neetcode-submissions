c2497d223b5d7da17cff781b1110af3ff6c576ed
Anant Joshi <anantjoshi97@users.noreply.github.com>
2026-05-07 15:17:20 -0500

    Add: house-robber - submission-9

---
+++ Data Structures & Algorithms/house-robber/submission-9.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (nums.size()==0){return 0;}
+        else if (nums.size()==1){return nums[0];}
+        else if (nums.size()==2){return std::max(nums[0],nums[1]);}
+
+        std::vector<int> rwd (n,0);
+        rwd[n-1] = nums[n-1];
+        rwd[n-2] = std::max(nums[n-1],nums[n-2]);
+
+        for (int i=n-3; i>=0; i--){
+            rwd[i] = std::max(rwd[i+1],nums[i]+rwd[i+2]);
+        }
+        return rwd[0];
+    }
+       
+};
